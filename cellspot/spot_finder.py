b1d684c901424dc1061c57b79b65b82ef1058491
heeler <jamie.sherman@gmail.com>
2024-08-19 20:04:46 -0700

    added docstrings

--- cellspot/spot_finder.py
+++ cellspot/spot_finder.py
@@ -5,8 +5,19 @@ from cellpose import models
 import numpy as np
 
 
-class SpotFinder:
+class SpotFinder(object):
     def __init__(self, image_path: Path) -> None:
+        """
+        Load the Tif file provided in image_path and set the
+        class internals to use the cyto3 model for image segmentation.
+        Parameters
+        ----------
+        image_path: Path to a Tif file with 3 channels / RGB in our case.
+
+        Returns
+        -------
+        None
+        """
         self.img_path = image_path
         self.image = tifffile.imread(self.img_path)
         self.model = models.Cellpose(gpu=False, model_type='cyto3')
@@ -14,6 +25,16 @@ class SpotFinder:
         self.keepers = None
 
     def create_cell_mask(self) -> np.ndarray:
+        """
+        This launches CellPose and captures the outputs.
+        The relevant output currently is the mask.
+
+        Returns
+        -------
+        The mask of the detected cells. Each cell found is
+        given an index and the pixels for that cell mask have
+        said integer value.
+        """
         masks, flows, styles, diams = self.model.eval(
             [self.image],
             channels=[3, 3],
@@ -27,6 +48,14 @@ class SpotFinder:
         return mask
 
     def keep_intersection(self) -> np.ndarray:
+        """
+        This function creates a list of the mask integers which
+        overlap a red pixel, aka an inclusion body.
+
+        Returns
+        -------
+        A unique list of mask integers.
+        """
         keepers = []
         with np.nditer([self.image[:,:,0], self.mask]) as it:
             for i, m in it:
@@ -37,6 +66,15 @@ class SpotFinder:
         return keepers
 
     def remove_empty_cells(self) -> np.ndarray:
+        """
+        This function creates a new mask consisting of
+        only those cell masks found by keep_intersection().
+
+        Returns
+        -------
+        A cell mask consisting of cell masks of cells
+        with inclusion bodies.
+        """
         with np.nditer([self.mask, None]) as it:
             for x, y in it:
                 ans = 0
